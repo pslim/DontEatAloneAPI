98b082e759a94a91b9495d48cfc8a7d2714a758b
Paige Lim <lim.paige@gmail.com>
2015-06-05 13:36:12 -0400

    update UserTableSeeder

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+class UsersTableSeeder extends Seeder {
+
+	public function run() {
+
+		Eloquent::unguard();	// to fill all fields
+
+		$faker = Faker\Factory::create();
+		$faker->seed(1234);
+
+		for ( $i = 0; $i < 200; $i++ ) {
+			User::create([
+				'email'			=>	$faker->email,
+				'password'		=>	Hash::make($faker->word)	
+			]);
+		}
+
+	}
+}
