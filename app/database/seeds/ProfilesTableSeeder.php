d7ccd0cdd61cabda8389eb3a38921878ee46b1df
Paige Lim <lim.paige@gmail.com>
2015-07-19 18:17:29 -0400

    1/10 dp images

--- app/database/seeds/ProfilesTableSeeder.php
+++ app/database/seeds/ProfilesTableSeeder.php
@@ -8,12 +8,17 @@ class ProfilesTableSeeder extends Seeder {
 		$faker = Faker\Factory::create();
 		$faker->seed(1234);
 
+		$n = 0;
 		for ( $i = 1; $i < 1000; $i++ ) {
+			$n++;
+			if ($n == 11) {
+				$n = 0;
+			}
 			Profile::create([
 				'user_id'		=>	$i,
 				'name'			=>	$faker->name,
 				// 'image_url'		=>	$faker->imageUrl(64, 64, 'cats', $i),
-				'image_url'		=>	'http://lorempixel.com/64/64/cats/'.$i,
+				'image_url'		=>	'http://lorempixel.com/64/64/cats/'.$n,
 				'gender'		=>	$faker->randomElement(['M', 'F']),
 				'age'			=>	$faker->numberBetween(10, 60),
 				'likes'			=>	$faker->numberBetween(0, 100),
